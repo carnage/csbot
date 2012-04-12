f0bb6023a3273e9abe78a9c078debf9699132a52
Alan Briolat <alan.briolat@gmail.com>
2012-04-12 12:09:47 +0100

    Make auth plugin use deferreds and prevent duplicate outstanding requests

--- csbot/plugins/auth.py
+++ csbot/plugins/auth.py
@@ -1,3 +1,5 @@
+from twisted.internet.defer import Deferred, inlineCallbacks
+
 from csbot.core import Plugin, PluginFeatures
 from csbot.util import nick
 
@@ -5,17 +7,40 @@ from csbot.util import nick
 class Auth(Plugin):
     features = PluginFeatures()
 
+    def setup(self):
+        self.account_queries = dict()
+
     @features.command('auth.check')
+    @inlineCallbacks
     def auth_check(self, event):
-        event.protocol.identify(event.data[0])
+        account = yield self.get_user_account(event, event.data[0])
+
+        if account:
+            event.reply('{user} is identified to {account}'.format(
+                user=event.data[0], account=account))
+        else:
+            event.reply('{user} is not identified'.format(
+                user=event.data[0]))
 
     @features.hook('userIdentified')
     def userIdentified(self, event):
-        if event.account:
-            event.protocol.msg(
-                    '#cs-york-dev',
-                    '{e.user} is identified to account '
-                    '{e.account}'.format(e=event))
+        """Fire any waiting deferreds when we find the account a user is
+        identified to.
+        """
+        deferreds = self.account_queries.pop(event.user, list())
+        for d in deferreds:
+            d.callback(event.account)
+
+    def get_user_account(self, event, user):
+        """Get a :class:`Deferred` that will yield the account name of *user*,
+        or None if *user* isn't identified or doesn't exist.
+        """
+        d = Deferred()
+        if user in self.account_queries:
+            # If there is an outstanding request, just add a "me too"
+            self.account_queries[user].append(d)
         else:
-            event.protocol.msg('#cs-york-dev',
-                               '{e.user} is not identified'.format(e=event))
+            # If there is no outstanding request, actually make the request
+            self.account_queries[user] = [d]
+            event.protocol.identify(user)
+        return d
